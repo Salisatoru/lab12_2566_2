a32b64d29f7edb568b1dbe4d0bcd65b636a59c30
Salisatoru <95071118+Salisatoru@users.noreply.github.com>
2023-12-26 16:10:13 +0700

    Update lab12_2.cpp

--- lab12_2.cpp
+++ lab12_2.cpp
@@ -1,6 +1,8 @@
 #include<iostream>
 using namespace std;
 
+int gcd( int x, int y);
+
 int main(){
     int x, y;
     cin >> x >> y;
@@ -12,3 +14,4 @@ int gcd( int x, int y){
     if (x%y==0) return y;
     else return gcd(y, x%y);
 }
+
